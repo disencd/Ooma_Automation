21e6a0d7939cbc7a0759a3aa9404fe3fb180ab5d
disencd <disencd@gmail.com>
2017-04-03 19:56:05 -0700

    Added Sensor Folder, GPIO code, Flask Server/client

---
+++ ooma-automation/ooma/homemonitoring/sensor/flask_webserver/bb_gpio.py
@@ -0,0 +1,18 @@
+
+
+class bb_gpio_process():
+    def __init__(self):
+        pass
+
+    def process_door_sensor_request(self, interface, switch_state):
+        response = "Going to Process the Door Sensor"
+        return response
+
+    def process_water_sensor_request(self, interface, switch_state):
+        response = "Going to Process the Water Sensor"
+        return response
+
+    def process_motion_sensor_request(self, interface, switch_state):
+        response = "Going to Process the Motion Sensor"
+        return response
+
