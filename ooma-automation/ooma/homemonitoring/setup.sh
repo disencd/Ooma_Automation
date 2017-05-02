8094a1aa2eaac868c13efa5757d6769a42356019
disencd <disencd@gmail.com>
2017-05-01 17:38:49 -0700

    added python path commenting

--- ooma-automation/ooma/homemonitoring/setup.sh
+++ ooma-automation/ooma/homemonitoring/setup.sh
@@ -6,6 +6,8 @@ python settings.py
 
 env
 
-PYTHONPATH="${PYTHONPATH}/Users/Shared/Jenkins/Home/workspace/HomeSecurity/ooma-automation/ooma/homemonitoring/"
-export PYTHONPATH
-env
+#PYTHONPATH="${PYTHONPATH}/Users/Shared/Jenkins/Home/workspace/HomeSecurity/ooma-automation/ooma/homemonitoring/"
+#export PYTHONPATH
+#env
+
+ln -s "/Users/Shared/Jenkins/Home/workspace/HomeSecurity/ooma-automation/ooma/homemonitoring/" /usr/lib/pymodules/python2.7/
