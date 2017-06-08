14e287e961c7a57ceb9dbb1aebee8e96eb1c64a6
disencd <disencd@gmail.com>
2017-06-07 17:31:21 -0700

    mongo logging added

--- ooma-automation/ooma/homemonitoring/setup/mongodb_setup.py
+++ ooma-automation/ooma/homemonitoring/setup/mongodb_setup.py
@@ -3,6 +3,8 @@ import pymongo
 from homemonitoring.setup.json_parse import JsonConfig
 import logging
 import colorlog
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 class MongoDBQuery():
     def __init__(self):
         self.json_obj = JsonConfig()
