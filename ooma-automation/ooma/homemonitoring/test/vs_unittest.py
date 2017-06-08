064dcaae2a32d86ead538ecbd4c85846dd8c178b
root <root@localhost.localdomain>
2017-06-08 13:06:49 -0700

    adding the fill dds message

--- ooma-automation/ooma/homemonitoring/test/vs_unittest.py
+++ ooma-automation/ooma/homemonitoring/test/vs_unittest.py
@@ -15,7 +15,7 @@ class VStest(unittest.TestCase):
         logger.info("Setting up Virtual Test Automation")
         self.custom_timers = {}
 
-    def abctest_1_activate_hms(self):
+    def abc_test_1_activate_hms(self):
         logger.info("Activating the OSS HMS Account")
         oss_hms = HMS_Activation()
         cnt = 0
