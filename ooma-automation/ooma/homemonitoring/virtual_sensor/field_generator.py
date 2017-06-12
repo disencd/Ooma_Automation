faec9097d62f1c46335c7bb7fbc89f9569cbc28d
disencd <disencd@gmail.com>
2017-06-12 14:46:10 -0700

    added mongo support for finding element

--- ooma-automation/ooma/homemonitoring/virtual_sensor/field_generator.py
+++ ooma-automation/ooma/homemonitoring/virtual_sensor/field_generator.py
@@ -75,7 +75,7 @@ class SensorNamegenerator():
     def generate_sensor_name(self, name, cust_pk):
         _mong_obj = MongoDBQuery()
         mongo_acc = _mong_obj.mongo_connect("SensorCount_collection")
-        cursor = mongo_acc.mongo_find({"_id" : cust_pk})
+        cursor = mongo_acc.mongo_find_one_element({"_id" : cust_pk})
 
         logger.info("Total %s Door %s"% (cursor["total"], cursor["door"]))
         return cursor
