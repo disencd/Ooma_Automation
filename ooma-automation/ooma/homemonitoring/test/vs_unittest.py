6fe24414c549a70ea5fbc9b450df00fa20cdddf8
disencd <disencd@gmail.com>
2017-06-13 17:47:44 -0700

    Pairing sensors from mongo DB

--- ooma-automation/ooma/homemonitoring/test/vs_unittest.py
+++ ooma-automation/ooma/homemonitoring/test/vs_unittest.py
@@ -4,6 +4,7 @@ import time, os
 from homemonitoring.virtual_sensor.create_oss_hms_acc import HMS_Activation
 from homemonitoring.virtual_sensor.pair_sensor import Sensor_Addition
 from homemonitoring.virtual_sensor.generate_sensor_events import Sensor_Action
+from homemonitoring.setup.mongodb_setup import MongoDBQuery
 import logging
 
 logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
