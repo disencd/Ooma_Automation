bdad8cf9af4f2ba19f68e9dd58d28c36cc76c44b
disencd <disencd@gmail.com>
2017-06-15 15:42:38 -0700

    dictionary update

--- ooma-automation/ooma/homemonitoring/test/vs_addsensor.py
+++ ooma-automation/ooma/homemonitoring/test/vs_addsensor.py
@@ -27,12 +27,9 @@ class VStest(unittest.TestCase):
         results = _mong_obj.mongo_return_elements()
         _mong_obj.mongo_disconnect()
 
-        cnt = 0
         for val in results:
-            if cnt < 1:
                 logger.info("pairing the PK with %s", val["cust_pk"])
                 sensor_add.pair_door_sensor(val["cust_pk"])
-                cnt += 1
         #id = "virtualaccount20170607170607296891"
 
 if __name__ == "__main__":
