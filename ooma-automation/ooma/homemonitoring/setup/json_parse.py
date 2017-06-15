60e03f4095d6723c7e5c5a7384f185c54b84c66a
disencd <disencd@gmail.com>
2017-06-14 19:43:29 -0700

    json ordering

--- ooma-automation/ooma/homemonitoring/setup/json_parse.py
+++ ooma-automation/ooma/homemonitoring/setup/json_parse.py
@@ -1,8 +1,8 @@
 import json
-
+from collections import OrderedDict
 class JsonConfig():
     #Reading the config file from Json File
     def dump_config(self, filename):
         with open(filename) as json_data_file:
-            data = json.load(json_data_file)
+            data = json.load(json_data_file, object_pairs_hook=OrderedDict)
             return data
