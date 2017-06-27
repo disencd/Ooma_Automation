26e014153184b913d380707811034a5c98ea2aa9
disencd <disencd@gmail.com>
2017-06-27 15:34:34 -0700

    resetting data points

--- ooma-automation/ooma/homemonitoring/setup/update_tables.py
+++ ooma-automation/ooma/homemonitoring/setup/update_tables.py
@@ -1,8 +1,14 @@
 import MySQLdb
+import logging, os
 from homemonitoring.setup.json_parse import JsonConfig
 from homemonitoring.setup.mongodb_setup import MongoDBQuery
 from homemonitoring.virtual_sensor.hms_sql_query import HMSSqlQuery
 from homemonitoring.virtual_sensor.generate_sensor_events import Sensor_Action
+logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+    datefmt='%d-%m-%Y:%H:%M:%S',
+    level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 class UpdateTables():
     def __init__(self):
