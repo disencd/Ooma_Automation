0ecc45a35dbf2b8aa29329255e6cee434d089701
disencd <disencd@gmail.com>
2017-06-27 23:54:12 -0700

    resetting data points

--- ooma-automation/ooma/homemonitoring/setup/mongodb_setup.py
+++ ooma-automation/ooma/homemonitoring/setup/mongodb_setup.py
@@ -4,7 +4,10 @@ from homemonitoring.setup.json_parse import JsonConfig
 
 import logging
 import colorlog
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.ERROR)
+logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+    datefmt='%d-%m-%Y:%H:%M:%S',
+    level=logging.ERROR)
 logger = logging.getLogger(__name__)
 
 class MongoDBQuery():
