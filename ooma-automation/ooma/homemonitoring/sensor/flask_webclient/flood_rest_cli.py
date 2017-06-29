c21da8f3345bd402fa441528ed95a326d50e4387
disencd <disencd@gmail.com>
2017-06-28 20:17:39 -0700

    log validation

--- ooma-automation/ooma/homemonitoring/sensor/flask_webclient/flood_rest_cli.py
+++ ooma-automation/ooma/homemonitoring/sensor/flask_webclient/flood_rest_cli.py
@@ -91,7 +91,7 @@ class FlaskClientWaterSensor:
             _tamper_url += "/sensor/Water/interface/tamper/switch/on"
             __http_response = urllib2.urlopen(_tamper_url)
             logger.info("water_sensor_tampering_disabled - %s", _tamper_url)
-            return "detects tampering"
+            return "is armed"
         except urllib2.URLError as e:
             logger.error("Error - BBFlaskServer is not running")
             return e.code
@@ -103,7 +103,7 @@ class FlaskClientWaterSensor:
             _tamper_url += "/sensor/Water/interface/tamper/switch/off"
             __http_response = urllib2.urlopen(_tamper_url)
             logger.info("water_sensor_tampering_enabled - %s", _tamper_url)
-            return "is armed"
+            return "detects tampering"
         except urllib2.URLError as e:
             logger.error("Error - BBFlaskServer is not running")
             return e.code
