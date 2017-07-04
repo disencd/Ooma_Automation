21736c88251c124dd83166b6bfd695a3a90663d2
disencd <disencd@gmail.com>
2017-07-04 10:47:24 -0700

    logging enables

--- ooma-automation/ooma/homemonitoring/virtual_sensor/create_oss_hms_acc.py
+++ ooma-automation/ooma/homemonitoring/virtual_sensor/create_oss_hms_acc.py
@@ -90,7 +90,7 @@ class HMS_Activation(object):
 
             if self.mongodb_dict["activation_status"] and self.mongo_enable == "enable":
                 _mong_obj.mongo_insertion("acc_collection", self.mongodb_dict)
-                logger.info("Getting Info of The HMS Account %s", response)
+                logger.info("Getting Info of The HMS Account %s", response[0]["id"])
                 _mong_obj.mongo_reset_sensor_count(response[0]["id"])
 
 
